a59d126a934de1fb3ede1044188b5a4f35eae9a5
Nathan Vieira <nathan.csvieira@gmail.com>
2023-09-05 15:44:43 -0300

    Segundo Teste com o GitHub

---
+++ teste.py
@@ -0,0 +1,8 @@
+# Solicita ao usuário que insira um número
+numero = float(input("Digite um número: "))
+
+# Calcula o dobro do número
+dobro = numero * 2
+
+# Imprime o dobro do número
+print(f"O dobro de {numero} é {dobro}")
